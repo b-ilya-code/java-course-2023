d63f9205818a02c23e1074c393e1223592a43e42
bykov-ilya2712 <bykov.ilya2712@gmail.com>
2023-10-13 13:10:17 +0300

    Task7 changed

--- src/main/java/edu/hw1/Task7.java
+++ src/main/java/edu/hw1/Task7.java
@@ -4,13 +4,25 @@ public class Task7 {
     private Task7() {
     }
 
-    private static final int BIT_DEPTH_INT = 32;
-
     public static int rotateLeft(int n, int shift) {
-        return (n << shift) | (n >>> (BIT_DEPTH_INT - shift));
+        if (shift < 0) {
+            return rotateRight(n, -shift);
+        }
+
+        final int BIT_DEPTH = getBitDepth(n);
+        return ~(-1 << BIT_DEPTH) & ((n << shift) | (n >>> (BIT_DEPTH - shift)));
     }
 
     public static int rotateRight(int n, int shift) {
-        return (n >>> shift) | (n << (BIT_DEPTH_INT - shift));
+        if (shift < 0) {
+            return rotateLeft(n, -shift);
+        }
+
+        final int BIT_DEPTH = getBitDepth(n);
+        return ~(-1 << BIT_DEPTH) & ((n >>> shift) | (n << (BIT_DEPTH - shift)));
+    }
+
+    private static int getBitDepth(int n) {
+        return (int) (Math.log(n) / Math.log(2)) + 1;
     }
 }
--- src/test/java/edu/hw1/Task7Test.java
+++ src/test/java/edu/hw1/Task7Test.java
@@ -6,13 +6,9 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 public class Task7Test {
     @Test
     void checkCyclicBitShift() {
-        //valid input
-        assertThat(Task7.rotateRight(1, 1)).isEqualTo(-2147483648);
-        assertThat(Task7.rotateLeft(1, 31)).isEqualTo(-2147483648);
-        assertThat(Task7.rotateLeft(1, 3)).isEqualTo(8);
-
-        //invalid input, but processed correctly
-        assertThat(Task7.rotateLeft(-2147483648, 1)).isEqualTo(1);
+        assertThat(Task7.rotateRight(8, 1)).isEqualTo(4);
+        assertThat(Task7.rotateLeft(16, 1)).isEqualTo(1);
+        assertThat(Task7.rotateLeft(17, 2)).isEqualTo(6);
         assertThat(Task7.rotateLeft(1, -1)).isEqualTo(Task7.rotateRight(1, 1));
         assertThat(Task7.rotateRight(1, -1)).isEqualTo(Task7.rotateLeft(1, 1));
     }
