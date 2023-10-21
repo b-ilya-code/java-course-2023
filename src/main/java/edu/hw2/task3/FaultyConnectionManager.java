a3c6cb5ab1530b62a47897e511b1cac9204bd684
bykov-ilya2712 <bykov.ilya2712@gmail.com>
2023-10-21 16:14:50 +0300

    Task3 + Task3Test

--- src/main/java/edu/hw2/task3/FaultyConnectionManager.java
+++ src/main/java/edu/hw2/task3/FaultyConnectionManager.java
@@ -3,6 +3,6 @@ package edu.hw2.task3;
 public class FaultyConnectionManager implements ConnectionManager {
     @Override
     public Connection getConnection() {
-
+        return new FaultyConnection();
     }
 }
