24157295ac1fcd1cec1cb81e1ee3b8b283f7f85f
bykov-ilya2712 <bykov.ilya2712@gmail.com>
2023-11-30 22:13:45 +0300

    tests added

--- src/test/java/edu/hw3/Task1Test.java
+++ src/test/java/edu/hw3/Task1Test.java
@@ -10,5 +10,6 @@ public class Task1Test {
         assertThat(Task1.atbash("AZ")).isEqualTo("ZA");
         assertThat(Task1.atbash("")).isEqualTo("");
         assertThat(Task1.atbash("123")).isEqualTo("123");
+        assertThat(Task1.atbash("Привет мир!")).isEqualTo("Привет мир!");
     }
 }
--- src/test/java/edu/hw3/Task4Test.java
+++ src/test/java/edu/hw3/Task4Test.java
@@ -1,15 +1,48 @@
 package edu.hw3;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 public class Task4Test {
-    @Test
-    void testConvertToRoman() {
-        assertThat(Task4.convertToRoman(2)).isEqualTo("II");
-        assertThat(Task4.convertToRoman(16)).isEqualTo("XVI");
+    @ParameterizedTest
+    @CsvSource({
+        "1, I",
+        "5, V",
+        "10, X",
+        "50, L",
+        "100, C",
+        "500, D",
+        "1000, M"
+    })
+    void testOneCharacterNumbers(int number, String expected) {
+        assertThat(Task4.convertToRoman(number)).isEqualTo(expected);
+    }
 
-        assertThat(Task4.convertToRoman(0)).isEqualTo("");
-        assertThat(Task4.convertToRoman(-1)).isEqualTo("");
+    @ParameterizedTest
+    @CsvSource({
+        "2, II",
+        "9, IX",
+        "16, XVI",
+        "19, XIX",
+        "41, XLI",
+        "49, XLIX",
+        "59, LIX",
+        "91, XCI",
+        "99, XCIX",
+        "391, CCCXCI",
+        "490, CDXC"
+    })
+    void testMultipleCharactersNumbers(int number, String expected) {
+        assertThat(Task4.convertToRoman(number)).isEqualTo(expected);
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+        "0,''",
+        "-1,''"
+    })
+    void testWrongInput(int number, String expected) {
+        assertThat(Task4.convertToRoman(number)).isEqualTo(expected);
     }
 }
